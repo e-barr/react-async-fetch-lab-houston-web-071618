4897a835c8947240a4925afc33a2848213bba18f
e-barr <github email address>
2018-09-21 08:56:35 -0500

    Done.

--- src/components/ExampleComponent.js
+++ src/components/ExampleComponent.js
@@ -2,10 +2,20 @@ import React, { Component } from 'react';
 
 class ExampleComponent extends Component {
 
+  generatePeopleDivs = () => {
+    return this.props.info.map(person => {
+      return (
+        <div>
+          <h2>Name: {person.name}</h2>
+          <h3>Craft: {person.craft}</h3>
+        </div>
+      )
+    })
+  }
   render() {
     return (
       <strong>
-        Example!
+        {this.generatePeopleDivs()}
       </strong>
     )
   }
